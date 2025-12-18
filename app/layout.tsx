55402a55810a5ab0763452aff20487475b43a6dd
Kaelyn Suh <kaelyn.suh@gmail.com>
2025-12-18 14:06:42 -0800

    Import font and define global css variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,34 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const ppNeueMachina = localFont({
+  src: "./fonts/PPNeueMachina-InktrapSemibold.woff2",
+  variable: "--font-pp-neue-machina",
+  weight: "600",
+  display: "swap",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const ppNeueMontreal = localFont({
+  src: [
+    {
+      path: "./fonts/PPNeueMontreal-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/PPNeueMontreal-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/PPNeueMontreal-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+  variable: "--font-pp-neue-montreal",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +44,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${ppNeueMachina.variable} ${ppNeueMontreal.variable} antialiased`}
       >
         {children}
       </body>
