c749a6d4f3d89f619b3a8b546e8e5e95d1451de1
Kaelyn Suh <kaelyn.suh@gmail.com>
2025-12-18 21:51:51 -0800

    Arrow component

--- app/components/Arrow.tsx
+++ app/components/Arrow.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+
 export function Arrow() {
-  return "->";
+  return (
+    <div className="py-4 px-0 sm:px-1 md:px-3 lg:px-5 xl:px-8 min-[1430px]:px-[45px]">
+      <Image
+        src="/right-arrow.svg"
+        alt="arrow"
+        width={52}
+        height={46}
+        className="w-8 md:w-10 lg:w-[52px] rotate-90 md:rotate-0"
+      />
+    </div>
+  );
 }
